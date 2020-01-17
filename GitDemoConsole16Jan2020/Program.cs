a99cc044a0552c3d7310cc60d390a204ee76af0a
saurabhambastha18 <saurabhambastha18@gmail.com>
2020-01-18 00:43:20 +0530

    Added by saurabhamb

--- GitDemoConsole16Jan2020/Program.cs
+++ GitDemoConsole16Jan2020/Program.cs
@@ -10,7 +10,7 @@ namespace GitDemoConsole16Jan2020
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Change by saurabhambastha18 at " + DateTime.Now.ToLongTimeString());
+			Console.WriteLine("1 Change by saurabhambastha18 at " + DateTime.Now.ToLongTimeString());
 		}
 	}
 }
